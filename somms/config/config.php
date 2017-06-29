d7278f90599246d521bc77b2af4508c009c39e01
Achuthkamath91 <achuth.kamath91@gmail.com>
2017-06-29 17:14:13 -0400

    added bootstrap css and js

--- somms/config/config.php
+++ somms/config/config.php
@@ -5,16 +5,16 @@ define('DBPASS', '');
 define('DBNAME', 'moss');
 if (!CMS) {
 	define('HOME_PATH', '/');
-	define('CONFIG_PATH', '/config/');
-	define('CSS_PATH', '/resources/css/');
-	define('IMAGES_PATH', '/resources/images/');
-	define('JS_PATH', '/resources/js/');
+	define('CONFIG_PATH', 'config/');
+	define('CSS_PATH', 'resources/css/');
+	define('IMAGES_PATH', 'resources/images/');
+	define('JS_PATH', 'resources/js/');
 	define('MODEL_PATH', 'model/');
 	define('CONTROLLER_PATH', 'controller/');
 	define('VIEW_PATH', 'view/');
 }else{
-	define('CSS_PATH', '/resources/css/');
-	define('IMAGES_PATH', '/resources/images/');
+	define('CSS_PATH', 'resources/css/');
+	define('IMAGES_PATH', 'resources/images/');
 	define('JS_PATH', '/resources/js/');
 	define('INCLUDES_PATH', '/cms/model/');
 }
--- somms/index.php
+++ somms/index.php
@@ -28,22 +28,22 @@ if(isset($res)){
 <html>
 	<head>
 		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="description" content="Free Web tutorials">
 		<meta name="keywords" content="Wine,review,best wines">
 		<meta name="author" content="Cetifiedsomms">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Cetifiedsomms</title>
-		<link rel="stylesheet" href="customStyle.css" />
+		<link rel="stylesheet" href=<?php echo CSS_PATH?>customStyle.css />
 		
 		 <!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href=<?php echo CSS_PATH?>bootstrap.min.css>
 
 		<!-- jQuery library -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src=<?php echo JS_PATH?>jquery-3.2.1.min.js></script>
 
 		<!-- Latest compiled JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+		<script src=<?php echo JS_PATH?>bootstrap.min.js></script> 
 		
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
